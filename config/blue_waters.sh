44990c9d34462b27bd58e37856f4894d5045f4df
Paul Young <yyang173@illinois.edu>
2018-01-15 14:25:53 -0600

    updated BW build script with GPU

--- config/blue_waters.sh
+++ config/blue_waters.sh
@@ -3,8 +3,9 @@
 ################################################################
 ## * This script builds available configurations of QMCPACK   ##
 ##   on Blue Waters, University of Illinois Urbnana-Champaign ##
+##   highly similar to OLCF Titan build script                ##
 ##                                                            ##
-## Last modified: Dec 20, 2017                                ##
+## Last modified: Jan 15, 2018                                ##
 ################################################################
 
 # Load required modules (assuming default settings have not been modified)
@@ -25,11 +26,11 @@ module load fftw
 module load boost
 module load libxml2
 module load cmake
-module load bwpy # numpy, h5py are libraries used in ctest
+module load bwpy # numpy, h5py libraries are used in ctest
 
 # use AMD optimized math libraries (performance critical!)
 XT_FLAGS="-DHAVE_AMDLIBM=1"
-# Set cmake variables, shared for cpu builds
+# Set cmake variables, shared for cpu builds; not used in gpu builds
 CMAKE_FLAGS="-D QMC_INCLUDE=/u/staff/rmokos/libs/amdlibm/include \
              -D QMC_EXTRA_LIBS=/u/staff/rmokos/libs/amdlibm/lib/static/libamdlibm.a"
 
@@ -43,27 +44,56 @@ export CC=cc
 export CXX=CC
 
 # Configure and build cpu real
+suffix=_cpu_real
 echo ""
 echo ""
 echo "building qmcpack for cpu real"
-mkdir build_cpu_real
-cd build_cpu_real
+mkdir build$suffix
+cd build$suffix
 cmake -D CMAKE_C_FLAGS="$XT_FLAGS" \
       -D CMAKE_CXX_FLAGS="$XT_FLAGS" \
       $CMAKE_FLAGS ..
 make -j 32
 cd ..
-ln -s ./build_cpu_real/bin/qmcpack ./qmcpack_cpu_real
+ln -s ./build$suffix/bin/qmcpack ./qmcpack$suffix
 
 # Configure and build cpu complex
+suffix=_cpu_comp
 echo ""
 echo ""
 echo "building qmcpack for cpu complex"
-mkdir build_cpu_comp
-cd build_cpu_comp
+mkdir build$suffix
+cd build$suffix
 cmake -D CMAKE_C_FLAGS="$XT_FLAGS" \
       -D CMAKE_CXX_FLAGS="$XT_FLAGS" \
       -D QMC_COMPLEX=1 $CMAKE_FLAGS ..
 make -j 32
 cd ..
-ln -s ./build_cpu_comp/bin/qmcpack ./qmcpack_cpu_comp
+ln -s ./build$suffix/bin/qmcpack ./qmcpack$suffix
+
+module load cudatoolkit
+# Configure and build gpu real
+suffix=_gpu_real
+echo ""
+echo ""
+echo "building qmcpack for gpu real"
+mkdir build$suffix
+cd build$suffix
+cmake -D QMC_CUDA=1 -DCUDA_HOST_COMPILER=$(which CC) ..
+cmake -D QMC_CUDA=1 -DCUDA_HOST_COMPILER=$(which CC) ..
+make -j 32
+cd ..
+ln -s ./build$suffix/bin/qmcpack ./qmcpack$suffix
+
+# Configure and build gpu complex
+suffix=_gpu_comp
+echo ""
+echo ""
+echo "building qmcpack for gpu real"
+mkdir build$suffix
+cd build$suffix
+cmake -D QMC_COMPLEX=1 -D QMC_CUDA=1 -DCUDA_HOST_COMPILER=$(which CC) ..
+cmake -D QMC_COMPLEX=1 -D QMC_CUDA=1 -DCUDA_HOST_COMPILER=$(which CC) ..
+make -j 32
+cd ..
+ln -s ./build$suffix/bin/qmcpack ./qmcpack$suffix
