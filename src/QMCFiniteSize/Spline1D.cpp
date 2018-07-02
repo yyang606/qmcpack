aeb3d869432f87585d7d15d412bd498339b28d6c
Paul Young <yyang173@illinois.edu>
2018-07-01 19:05:05 -0500

    Revert "give Spline1D contructor control over boundary"
    This reverts commit 601c20effe482b5b85ac7a1278f0695bd65e5968.
    Spline1D seg. fault

--- src/QMCFiniteSize/Spline1D.cpp
+++ src/QMCFiniteSize/Spline1D.cpp
@@ -2,16 +2,12 @@
 using namespace std;
 Spline1D::Spline1D(vector<double> pts, vector<double> vals)
 {
+  grid1d_ = create_general_grid(pts.data(), pts.size());
   BCtype_d bc;
   bc.lVal = 0.0;
   bc.rVal = 0.0;
   bc.lCode=DERIV1;
   bc.rCode=DERIV1;
-  Spline1D(pts, vals, bc);
-}
-Spline1D::Spline1D(vector<double> pts, vector<double> vals, BCtype_d bc)
-{
-  grid1d_ = create_general_grid(pts.data(), pts.size());
   spline1d_ = create_NUBspline_1d_d(grid1d_, bc, vals.data());
 }
 double Spline1D::operator()(double x)
