0b181e39fae3afb1f02d1e88171474a4e1a5a635
Yubo "Paul" Yang <pyang@flatironinstitute.org>
2022-04-30 15:58:05 -0400

    fix tri test

--- src/QMCHamiltonians/CoulombPBCAA.cpp
+++ src/QMCHamiltonians/CoulombPBCAA.cpp
@@ -310,7 +310,7 @@ void CoulombPBCAA::initBreakup(ParticleSet& P)
   for (int spec = 0; spec < NumSpecies; spec++)
   {
     Zspec[spec]      = tspecies(ChargeAttribIndx, spec);
-    NofSpecies[spec] = static_cast<int>(tspecies(MemberAttribIndx, spec));
+    NofSpecies[spec] = P.groupsize(spec);
   }
   SpeciesID.resize(NumCenters);
   for (int iat = 0; iat < NumCenters; iat++)
