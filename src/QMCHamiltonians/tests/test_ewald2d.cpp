3825e510af26096cdc56d6741f4fa500f709d89c
Yubo "Paul" Yang <pyang@flatironinstitute.org>
2022-05-01 10:45:50 -0400

    body center

--- src/QMCHamiltonians/tests/test_ewald2d.cpp
+++ src/QMCHamiltonians/tests/test_ewald2d.cpp
@@ -42,4 +42,42 @@ TEST_CASE("Coulomb PBC A-A Ewald2D square", "[hamiltonian]")
   CHECK(val == Approx(vmad_sq));
 }
 
+TEST_CASE("Coulomb PBC A-A Ewald2D body center", "[hamiltonian]")
+{
+  LRCoulombSingleton::CoulombHandler = 0; // !!!! crucial if not first test
+  LRCoulombSingleton::this_lr_type = LRCoulombSingleton::STRICT2D;
+  const double vmad_bc = -2.7579038;
+  CrystalLattice<OHMMS_PRECISION, OHMMS_DIM> lattice;
+  lattice.BoxBConds = true;
+  lattice.BoxBConds[2] = false; // ppn
+  lattice.ndim = 2;
+  lattice.R.diagonal(1.0);
+  lattice.LR_dim_cutoff = 30.0;
+  lattice.reset();
+
+  ParticleSet elec;
+  elec.Lattice = lattice;
+  elec.setName("e");
+  const int npart = 2;
+  elec.create({npart});
+  elec.R[0] = {0.0, 0.0, 0.0};
+  elec.R[1] = {0.5, 0.5, 0.5};
+
+  SpeciesSet& tspecies       = elec.getSpeciesSet();
+  int upIdx                  = tspecies.addSpecies("u");
+  int chargeIdx              = tspecies.addAttribute("charge");
+  int massIdx                = tspecies.addAttribute("mass");
+  tspecies(chargeIdx, upIdx) = -1;
+  tspecies(massIdx, upIdx)   = 1.0;
+
+  elec.createSK();
+  elec.addTable(elec);
+  elec.update();
+
+  CoulombPBCAA caa = CoulombPBCAA(elec, true);
+
+  double val = caa.evaluate(elec);
+  CHECK(val/npart == Approx(vmad_bc));
+}
+
 } // qmcplusplus
