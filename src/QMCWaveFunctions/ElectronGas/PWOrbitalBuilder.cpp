c611907a10efdae30d94e9380574d97501ae6a9b
Yubo "Paul" Yang <pyang@flatironinstitute.org>
2022-06-29 10:58:01 -0400

    initialize twistd kgrid for PW orbitals

--- src/QMCWaveFunctions/ElectronGas/PWOrbitalBuilder.cpp
+++ src/QMCWaveFunctions/ElectronGas/PWOrbitalBuilder.cpp
@@ -20,24 +20,29 @@ SPOSet* PWOrbitalBuilder::createSPOSetFromXML(xmlNodePtr cur)
   attrib.add(twist, "twist");
   attrib.put(cur);
 
+  auto lattice = targetPtcl.Lattice;
+
 #ifdef QMC_COMPLEX
   npw = norb;
 #else
   npw = std::ceil((norb+1.0)/2);
 #endif
   targetPtcl.setTwist(twist);
-  PosType tvec = targetPtcl.Lattice.k_cart(twist);
+  PosType tvec = lattice.k_cart(twist);
   app_log() << "twist fraction = " << twist << std::endl;
   app_log() << "twist cartesian = " << tvec << std::endl;
 
   // extract npw k-points from container
   // kpts_cart is sorted by magnitude
-  std::vector<PosType> kpts;
-  kpts.resize(npw);
+  std::vector<PosType> kpts(npw);
+  KContainer klists;
+  RealType kcut = lattice.LR_kc; // to-do: reduce kcut to >~ kf
+  klists.UpdateKLists(lattice, kcut, twist, lattice.ndim);
+
   kpts[0] = tvec;
   for (int ik=1;ik<npw;ik++)
   {
-    kpts[ik] = tvec+targetPtcl.SK->KLists.kpts_cart[ik-1];
+    kpts[ik] = tvec+klists.kpts_cart[ik-1];
   }
   PWOrbital* sposet = new PWOrbital(kpts);
   sposet->report("  ");
