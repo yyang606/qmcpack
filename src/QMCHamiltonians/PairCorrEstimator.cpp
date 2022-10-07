6a23613cd453c6e694c9e725ff3f4fb3cb39314c
Yubo "Paul" Yang <pyang@flatironinstitute.org>
2022-10-06 21:33:52 -0400

    fix gofr pair index

--- src/QMCHamiltonians/PairCorrEstimator.cpp
+++ src/QMCHamiltonians/PairCorrEstimator.cpp
@@ -104,6 +104,14 @@ PairCorrEstimator::PairCorrEstimator(ParticleSet& elns, std::string& sources)
 
 void PairCorrEstimator::resetTargetParticleSet(ParticleSet& P) {}
 
+// The value should match the index to norm_factor in set_norm_factor
+int PairCorrEstimator::gen_pair_id(const int ig, const int jg, const int ns){
+  if (jg < ig)
+    return ns * (ns - 1) / 2 - (ns - jg) * (ns - jg - 1) / 2 + ig;
+  else
+    return ns * (ns - 1) / 2 - (ns - ig) * (ns - ig - 1) / 2 + jg;
+}
+
 PairCorrEstimator::Return_t PairCorrEstimator::evaluate(ParticleSet& P)
 {
   BufferType& collectables(P.Collectables);
@@ -119,7 +127,7 @@ PairCorrEstimator::Return_t PairCorrEstimator::evaluate(ParticleSet& P)
       {
         const int loc     = static_cast<int>(DeltaInv * r);
         const int jg      = P.GroupID[j];
-        const int pair_id = ig * (ig + 1) / 2 + jg;
+        const int pair_id = gen_pair_id(ig, jg, num_species);
         collectables[pair_id * NumBins + loc + myIndex] += norm_factor(pair_id + 1, loc);
       }
     }
