0e77dcdf6164f3e24f44377b3c6c5bd4ac805aea
Yubo "Paul" Yang <pyang@flatironinstitute.org>
2023-08-29 13:47:06 -0400

    [ewald] better default alpha

--- src/Particle/LongRange/EwaldScreen2D.cpp
+++ src/Particle/LongRange/EwaldScreen2D.cpp
@@ -23,7 +23,8 @@ EwaldScreen2D::EwaldScreen2D(ParticleSet& ref, mRealType kc_in)
     throw std::runtime_error("screened Ewald requires distance_to_gate");
   LR_rc = ref.getLattice().LR_rc; // CoulombPBC needs get_rc() to createSpline4RbyVs
   LR_kc = ref.getLattice().LR_kc; // get_kc() is used in QMCFiniteSize
-  alpha = std::sqrt(LR_kc/2.0/LR_rc);
+  const mRealType alpha0 = std::sqrt(LR_kc/2.0/LR_rc);
+  alpha = std::max(0.61093226575644/dgate, alpha0);
   area = ref.getLattice().Volume/ref.getLattice().R(2,2);
   // report
   app_log() << "    alpha = " << alpha << " area = " << area << std::endl;
